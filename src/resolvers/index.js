dd1ff321425f900542be001eea175b0080096405
beataj <beata@emc2wave.com>
2021-02-22 14:37:58 -0800

    rm file folders

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -3,8 +3,7 @@ import Query from './Query';
 import Mutation from './Mutation';
 import Subscription from './Subscription';
 import User from './User';
-import Post from './Post';
-import Comment from './Comment';
+
 
 const resolvers = {
   Query,
